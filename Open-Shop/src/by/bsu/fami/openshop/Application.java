4a66a6f07614a41f1a4384fa44ce1d731b9ab4ca
Pavel Perestoronin <eigenein@gmail.com>
2010-12-07 03:10:29 +0200

    Added logging & resource handling.

--- Open-Shop/src/by/bsu/fami/openshop/Application.java
+++ Open-Shop/src/by/bsu/fami/openshop/Application.java
@@ -1,5 +1,9 @@
 package by.bsu.fami.openshop;
 
+import java.util.logging.*;
+
+import by.bsu.fami.openshop.resources.*;
+
 /**
  * The application entry point.
  * @author eigenein
@@ -7,8 +11,11 @@ package by.bsu.fami.openshop;
  */
 public class Application {
 
+	private static final Logger logger = 
+		Logger.getLogger(Application.class.getName());
+	
 	public static void main(String[] args) {
-		
+		logger.info(ResourcesProvider.get().getString("openshop.log.starting"));
 	}
 
 }
